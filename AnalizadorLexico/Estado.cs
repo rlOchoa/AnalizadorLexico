123edcf858a1afc48422f3128360c96d00f9e48f
Satoxehclim <xehclim1@gmail.com>
2024-04-05 18:24:20 -0600

    Clases Transición y Estado
    Se han agregado las clases de Transicion y estado

---
+++ AnalizadorLexico/Estado.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico
+{
+    internal class Estado
+    {
+        //Atributos de Estado
+        static int contadorIdEstado = 0;
+        private int idEstado1;
+        private bool edoAccept1;
+        private int token1;
+        private HashSet<Transicion> trans1 = new HashSet<Transicion>();
+
+        //Constructor de Estado
+        public Estado()
+        {
+            edoAccept1 = false;
+            token1 = -1;
+            idEstado1 = contadorIdEstado++;
+            trans1.Clear();
+        }
+
+        //Metodos GET y SET
+        public int getIdEstado()
+        {
+            return idEstado1;
+        }
+
+        public void setIdEstado(int idEstado)
+        {
+            idEstado1 = idEstado;
+        }
+
+        public bool getEdoAccept() 
+        {
+            return edoAccept1;
+        }
+
+        public void setEdoAccept(bool edoAccept)
+        {
+            edoAccept1 = edoAccept;
+        }
+
+        public int getToken()
+        {
+            return token1;
+        }
+
+        public void setToken(int token)
+        {
+            token1 = token;
+        }
+
+        public HashSet<Transicion> getTrans()
+        {
+            return trans1;
+        }
+
+        public void setTrans(HashSet<Transicion> trans)
+        {
+            trans1 = trans;
+        }
+    }
+}
---
+++ AnalizadorLexico/Transicion.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorLexico
+{
+    internal class Transicion
+    {
+        //Atributos Transicion
+        private char simSup1;
+        private char simInf1;
+        private Estado Edo1;
+
+        //constructores
+        public Transicion(char simSup, char simInf, Estado e)
+        {
+            simSup1 = simSup;
+            simInf1 = simInf;
+            Edo1 = e;
+        }
+        public Transicion(char simb, Estado e)
+        {
+            simSup1 = simb;
+            simInf1 = simb;
+            Edo1 = e;
+        }
+        public Transicion()
+        {
+            Edo1 = null;
+        }
+
+        //Metodos GET y SET
+        public char getSimSup()
+        {
+            return simSup1;
+        }
+
+        public void setSimSup(char simSup)
+        {
+            simSup1 = simSup;
+        }
+
+        public char getSimInf()
+        {
+            return simInf1;
+        }
+
+        public void setSimInf(char simInf)
+        {
+            simInf1 = simInf;
+        }
+
+        public Estado getEstado()
+        {
+            return Edo1;
+        }
+
+        public void setEstado(Estado e)
+        {
+            Edo1 = e;
+        }
+        //Metodos
+        public void setTransicion(char s1,char s2,Estado e)
+        {
+            simSup1 = s1;
+            simInf1 = s2;
+            Edo1 = e;
+        }
+
+        public void setTransicion(char s1, Estado e)
+        {
+            simSup1 = s1;
+            simInf1 = s1;
+            Edo1 = e;
+        }
+
+        public Estado getEdoTrans(char s)
+        {
+            if (simInf1 <= s && simSup1 >= s)
+            {
+                return Edo1;
+            }
+            return null;
+        }
+    }
+}
