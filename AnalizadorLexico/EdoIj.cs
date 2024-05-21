f70eca21c25b56dec726599bbc9097bf6cf8906a
zVioletta <92064685+zVioletta@users.noreply.github.com>
2024-05-20 18:19:13 -0600

    Actualizaciónd el TODO en linea 473
    Implementación de la función IntersectarEstados parida del EdoIJ

--- AnalizadorLexico/EdoIj.cs
+++ AnalizadorLexico/EdoIj.cs
@@ -9,15 +9,16 @@ namespace AnalizadorLexico
     public class EdoIj
     {
         //atributos
-        public int j;
-        public HashSet<Estado> conIj;
-        public int[] transicionesAFD = new int[257];
+        public int j { get; set; };
+        public HashSet<Estado> conIj { get; set; };
+        public int[] transicionesAFD { get; set; };
 
         public EdoIj()
         {
             j = -1;
             conIj = new HashSet<Estado>();
             conIj.Clear();
+            transicionesAFD = new int[257];
             for (int k = 0; k < 257; k++)
             {
                 transicionesAFD[k] = -1;
